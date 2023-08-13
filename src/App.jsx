67a4ef5eded84a40d392f6dc48fc0f1260158e2f
Luqmaan Y <luqmaany2015@gmail.com>
2023-08-13 19:07:11 +0300

    Global Header and Footer test

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,20 @@ import Login from './components/login/Login';
 import Portfolio from './components/portfolio/Portfolio';
 import About from './components/about/About';
 import Home from './components/home/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path="/" element={<Home />}/>
+        <Route path="/home" element={<Home />}/>
         <Route path="/portfolio" element={<Portfolio />}/>
         <Route path="/about" element={<About />}/>
         <Route path="/contact" element={<Login />}/>
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,18 +1,14 @@
 import { Col, Jumbotron, Row } from "react-bootstrap";
-import Header from "../Header";
 import Login from "../login/Login";
-import Footer from "../Footer";
 import CardExample from "../CardExample";
 
 const Home = (props) => {
   return (
     <Jumbotron>
-      <Header />
       <Row>
         <Col>   <Login />       </Col>
         <Col>   <CardExample /> </Col>
       </Row>
-      <Footer />
     </Jumbotron>
   );
 }
